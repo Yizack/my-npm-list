1c5783c756895448b1f212343d3775f0e3c681d6
Yizack Rangel <yizackr@gmail.com>
2023-06-13 19:25:23 +0200

    fix: scoped packages

--- server/api/packages/stats/[...name].get.js
+++ server/api/packages/stats/[...name].get.js
@@ -2,9 +2,9 @@ import { eq, desc } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
   const { name } = getRouterParams(event);
+  const packageName = name.split(",").join("/");
   const DB = useDb();
-
-  const pkg = await DB.select().from(tables.packages).where(eq(tables.packages.name, name)).get();
+  const pkg = await DB.select().from(tables.packages).where(eq(tables.packages.name, packageName)).get();
   const users = await DB.select({
     ghId: tables.users.ghId,
     ghUser: tables.users.ghUser,
