26f170c9e80d9e74d8ff640ddce7d00d88765c32
Yizack Rangel <yizackr@gmail.com>
2023-06-13 18:53:23 +0200

    chore: package page progress

---
+++ pages/package/[name].vue
@@ -0,0 +1,9 @@
+<script setup>
+const { loggedIn, user } = useUserSession();
+const { params } = useRoute();
+const { data: pkg } = await useFetch(`/api/packages/stats/${params.name}`);
+</script>
+
+<template>
+  {{ pkg }}
+</template>
---
+++ server/api/packages/stats/[name].get.js
@@ -0,0 +1,16 @@
+import { eq, desc } from "drizzle-orm";
+
+export default defineEventHandler(async (event) => {
+  const { name } = getRouterParams(event);
+  const DB = useDb();
+
+  const pkg = await DB.select().from(tables.packages).where(eq(tables.packages.name, name)).get();
+  const users = await DB.select({
+    ghId: tables.users.ghId,
+    ghUser: tables.users.ghUser,
+    ghName: tables.users.name,
+    count: tables.lists.count
+  }).from(tables.lists).innerJoin(tables.users, eq(tables.lists.ghId, tables.users.ghId)).where(eq(tables.lists.packageId, pkg.id)).orderBy(desc(tables.lists.count)).limit(50).all();
+
+  return { ...pkg, users };
+});
