096cc61cf7bcb35efca4ff666dae2df257000756
Yizack Rangel <yizackr@gmail.com>
2023-06-15 06:43:49 +0200

    chore: better stats

--- pages/stats.vue
+++ pages/stats.vue
@@ -6,10 +6,16 @@ const { data: counters } = useFetch("/api/stats");
   <main>
     <div class="row g-2">
       <div v-for="(counter, i) of counters" :key="i" class="col-lg-3 col-md-4 col-sm-6">
-        <div class="bg-body-tertiary rounded-3 p-3 p-lg-4 h-100">
-          <div class="text-center">
-            <h1>{{ counter.value }}</h1>
+        <div class="bg-body-tertiary rounded-3 p-3 p-lg-4 h-100 d-flex position-relative">
+          <div class="text-center m-auto">
+            <h1><strong>{{ counter.value }}</strong></h1>
+            <h4 v-if="counter.ref">
+              <NuxtLink :to="`/${counter.type}/${counter.ref}`">{{ counter.ref }}</NuxtLink>
+            </h4>
             <h5 class="text-muted m-0">{{ counter.description }}</h5>
+          </div>
+          <div class="position-absolute top-0 end-0 my-3 mx-3">
+            <span v-if="counter.ref" class="bg-body rounded-pill px-2 py-1">{{ counter.type }}</span>
           </div>
         </div>
       </div>
--- server/api/stats.js
+++ server/api/stats.js
@@ -1,52 +1,76 @@
-import { sql, desc } from "drizzle-orm";
+import { sql, desc, eq } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
   const DB = useDb();
   const packages = DB.select({ pCount: sql`COUNT(*)`.as("p_count") }).from(tables.packages).as("p");
   const users = DB.select({ uCount: sql`COUNT(*)`.as("u_count") }).from(tables.users).as("u");
   const lists = DB.select({ lCount: sql`COUNT(*)`.as("l_count") }).from(tables.lists).as("l");
+
   const mp = DB.select({
-    ghId: tables.lists.ghId,
+    name: sql`${tables.users.ghUser}`.as("name_p"),
     maxP: sql`COUNT(*)`.as("max_p")
-  }).from(tables.lists).groupBy(tables.lists.ghId).orderBy(desc(2)).limit(1).as("mp");
+  }).from(tables.lists).innerJoin(tables.users, eq(tables.lists.ghId, tables.users.ghId)).groupBy(tables.lists.packageId).groupBy(tables.lists.ghId).orderBy(desc(2)).limit(1).as("mp");
+
   const mc = DB.select({
-    ghId: tables.lists.ghId,
-    maxC: sql`MAX(${tables.lists.count})`.as("max_c")
-  }).from(tables.lists).as("mc");
+    name: sql`${tables.packages.name}`.as("name_c"),
+    count: sql`MAX(${tables.lists.count})`.as("max_c")
+  }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).as("mc");
+
+  const mu = DB.select({
+    name: sql`${tables.packages.name}`.as("name_u"),
+    count: sql`COUNT(*)`.as("max_u")
+  }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(2)).limit(1).as("mu");
 
   const counters = await DB.select({
     packages: packages.pCount,
     users: users.uCount,
     lists: lists.lCount,
-    maxP: mp.maxP,
-    maxC: mc.maxC
-  }).from([packages, users, lists, mp, mc]).get();
+    max: {
+      mostPackageCount: {
+        ref: mp.name,
+        count: mp.maxP
+      },
+      usedPackageNumber: {
+        ref: mc.name,
+        count: mc.count
+      },
+      usedPackageUsers: {
+        ref: mu.name,
+        count: mu.count
+      }
+    }
+  }).from([packages, users, lists, mp, mc, mu]).get();
 
   return [
     {
-      name: "Packages",
       value: counters.packages,
       description: "Unique packages fetched by all users"
     },
     {
-      name: "Users",
       value: counters.users,
-      description: "Number of users registered"
+      description: "Number of registered users"
     },
     {
-      name: "Packages in lists",
       value: counters.lists,
       description: "Total number of packages in all users list"
     },
     {
-      name: "User with most packages",
-      value: counters.maxP,
-      description: "Packages count of the user with the most packages in their list"
+      value: counters.max.mostPackageCount.count,
+      ref: counters.max.mostPackageCount.ref,
+      type: "user",
+      description: "Package count of the user with the most packages in their list"
     },
     {
-      name: "Single package used most by an user",
-      value: counters.maxC,
-      description: "Maximum number of times a single package has been utilized by a user"
+      value: counters.max.usedPackageNumber.count,
+      ref: counters.max.usedPackageNumber.ref,
+      type: "package",
+      description: "Maximum number of times a single package has been used by a user"
+    },
+    {
+      value: counters.max.usedPackageUsers.count,
+      ref: counters.max.usedPackageUsers.ref,
+      type: "package",
+      description: "Number of users who have used the most used package"
     }
   ];
 });
