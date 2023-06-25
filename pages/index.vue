752cfec45e73ff66f9ab5785e1dd32e80294fda4
Yizack Rangel <yizackr@gmail.com>
2023-06-25 16:58:08 +0200

    chore: fix typo

--- pages/index.vue
+++ pages/index.vue
@@ -74,7 +74,7 @@ const { data: start } = await useFetch("/api/start");
           <div class="col-12">
             <div class="bg-body-tertiary rounded-3 p-3 p-lg-4">
               <h2>Popular packages</h2>
-              <p>Top 5 packages by number of users.</p>
+              <p>Top 5 packages by number of users</p>
               <div v-for="pkg of start.packages_users" :key="pkg.name">
                 <div class="d-flex gap-2 align-items-center">
                   <Icon class="flex-shrink-0" name="solar:box-bold" size="1.3em" />
@@ -85,7 +85,7 @@ const { data: start } = await useFetch("/api/start");
                 </div>
               </div>
               <hr>
-              <p>Top 5 packages by number of users.</p>
+              <p>Top 5 packages by times used</p>
               <div v-for="pkg of start.packages_used" :key="pkg.name">
                 <div class="d-flex gap-2 align-items-center">
                   <Icon class="flex-shrink-0" name="solar:box-bold" size="1.3em" />
