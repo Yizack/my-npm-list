3012ec80d4ecb3f4f8aa2a2074d899a7cd0cedbe
Yizack Rangel <yizackr@gmail.com>
2023-06-25 16:58:20 +0200

    fix: await

--- server/api/start.get.js
+++ server/api/start.get.js
@@ -5,12 +5,12 @@ export default defineEventHandler(async () => {
   const users = await DB.select().from(tables.users).orderBy(desc(tables.users.joined)).limit(10).all();
 
   const packages_users = await DB.select({
-    name: sql`${tables.packages.name}`,
+    name: tables.packages.name,
     count: sql`COUNT(*)`
   }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(sql`COUNT(*)`)).limit(5).all();
 
-  const packages_used = DB.select({
-    name: sql`${tables.packages.name}`,
+  const packages_used = await DB.select({
+    name: tables.packages.name,
     count: sql`SUM(${tables.lists.count})`
   }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(sql`SUM(${tables.lists.count})`)).limit(5).all();
 
