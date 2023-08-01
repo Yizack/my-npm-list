73f3bfe348f1e4dfec6a9f345d965fb8222d8907
Yizack Rangel <yizackr@gmail.com>
2023-08-01 04:59:01 -0500

    chore: date of fetch orderby

--- server/api/start.get.js
+++ server/api/start.get.js
@@ -7,12 +7,12 @@ export default defineEventHandler(async () => {
   const packages_users = await DB.select({
     name: tables.packages.name,
     count: sql`COUNT(*)`
-  }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(sql`COUNT(*)`)).limit(5).all();
+  }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(tables.packages.lastFetch), desc(sql`COUNT(*)`)).limit(5).all();
 
   const packages_used = await DB.select({
     name: tables.packages.name,
     count: sql`SUM(${tables.lists.count})`
-  }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(sql`SUM(${tables.lists.count})`)).limit(5).all();
+  }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(tables.packages.lastFetch), desc(sql`SUM(${tables.lists.count})`)).limit(5).all();
 
   return { users, packages_users, packages_used };
 });
