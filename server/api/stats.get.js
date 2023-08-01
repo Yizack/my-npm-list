dae1f6d8cfc2d7e21cc6afdeb0e78fe602d035ab
Yizack Rangel <yizackr@gmail.com>
2023-08-01 05:26:27 -0500

    chore: order by fetch date

--- server/api/stats.get.js
+++ server/api/stats.get.js
@@ -20,7 +20,7 @@ export default defineEventHandler(async () => {
   const mu = DB.select({
     name: sql`${tables.packages.name}`.as("name_u"),
     count: sql`COUNT(*)`.as("max_u")
-  }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(sql`COUNT(*)`)).limit(1).as("mu");
+  }).from(tables.lists).innerJoin(tables.packages, eq(tables.lists.packageId, tables.packages.id)).groupBy(tables.lists.packageId).orderBy(desc(tables.packages.lastFetch), desc(sql`COUNT(*)`)).limit(1).as("mu");
 
   const counters = await DB.select({
     packages: packages.pCount,
